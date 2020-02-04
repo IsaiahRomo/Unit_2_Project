bfade06608c8bf5ab74eac6866aa2d8a155122b6
Isaiah Romo <iromo237@west-mec.org>
2020-02-03 18:12:39 -0700

    finished case 1

--- case1/tc_cart.js
+++ case1/tc_cart.js
@@ -11,4 +11,34 @@
    Filename: tc_cart.js
 	
 */
+// sets total to 0 and adds a row of the categories that are going to be listed
+var orderTotal = 0;
+var cartHTML = "<table>";
+cartHTML += "<tr>";
+cartHTML += "<th>Item</th>";
+cartHTML += "<th>Description</th>";
+cartHTML += "<th>Price</th>";
+cartHTML += "<th>Qty</th>";
+cartHTML += "<th>Total</th>";
 
+// calculates the prices of the item gets description and quantity
+for(var i = 0; i < item.length; i++){
+   cartHTML += "<tr>";
+   cartHTML += "<td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
+   cartHTML += "<td>" + itemDescription[i] + "</td>";
+   cartHTML += "<td>$" + itemPrice[i] + "</td>";
+   cartHTML += "<td>" + itemQty[i] + "</td>";
+   // mutiplay the cost and the amount thats being bought
+   var itemCost = itemPrice[i] * itemQty[i];
+   cartHTML += "<td>$" + itemCost + "</td>";
+   cartHTML += "</tr>";
+   orderTotal += itemCost;
+}
+
+// adds the total to the of the order to the Html
+cartHTML += "<tr>";
+cartHTML += "<td colspan='4'>SubTotal</td>";
+cartHTML += "<td>$" + orderTotal + "</td>";
+cartHTML += "</tr>";
+cartHTML += "</table>";
+document.getElementById("cart").innerHTML = cartHTML;
--- case1/tc_order.js
+++ case1/tc_order.js
@@ -16,4 +16,12 @@
    The itemQty array contains the quantity ordered of each item
    
 */
+// sets the arrays up with there values or strings
+var item = [10582, 23015, 41807, 10041];
+var itemDescription = ["1975 Green Bay Packers Football (signed), Item 10582", 
+                        "Tom Landry 1955 Football Card (undigned), Item 23015", 
+                        "1916 Army-Navy Game, Framed Photo (signed), Item 41807", 
+                        "Protective Card Sheets, Item 10041"];
+var itemPrice = [149.93, 89.98, 334.93, 22.67];
+var itemQty = [1, 1, 1, 4];
 
